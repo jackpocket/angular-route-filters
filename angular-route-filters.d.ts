e12da8d9a1478ebbe7cf844e1e402077b1936271
gabrielcatalin <catalin.troia@gmail.com>
2015-09-22 21:11:13 -0400

    Create new dists

--- angular-route-filters.d.ts
+++ angular-route-filters.d.ts
@@ -2,7 +2,3 @@ declare module 'angular-route-filters' {
 	import main = require('angular-route-filters/index');
 	export = main;
 }
-declare module 'angular-route-filters' {
-	import main = require('angular-route-filters/index');
-	export = main;
-}
