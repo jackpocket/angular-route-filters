cab902b4ed56a2a103a9bc59909cacc9dbdd11c6
gabrielcatalin <catalin.troia@gmail.com>
2015-09-24 16:24:01 -0400

    Make tests run again

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration
 // Generated on Fri Jun 19 2015 00:58:41 GMT-0400 (EDT)
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -15,18 +15,42 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/**/*.js'
+      'node_modules/sinon/pkg/sinon.js',
+      'node_modules/jasmine-sinon/lib/jasmine-sinon.js',
+      'app/vendor/es6-promise/promise.js',
+      'test/**/*.ts'
     ],
 
 
     // list of files to exclude
-    exclude: [
-    ],
+    exclude: [],
 
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      '**/*.ts': ['typescript']
+    },
+
+    typescriptPreprocessor: {
+      // options passed to the typescript compiler
+      options      : {
+        sourceMap     : false, // (optional) Generates corresponding .map file.
+        target        : 'ES5', // (optional) Specify ECMAScript target version: 'ES3' (default), or 'ES5'
+        //module: 'amd', // (optional) Specify module code generation: 'commonjs' or 'amd'
+        //noImplicitAny: true, // (optional) Warn on expressions and declarations with an implied 'any' type.
+        //noResolve     : true, // (optional) Skip resolution and preprocessing.
+        removeComments: true // (optional) Do not emit comments to output.
+      },
+      // extra typing definitions to pass to the compiler (globs allowed)
+      typings      : [
+        'typings/jasmine/jasmine.d.ts',
+        'typings/sinon/sinon.d.ts'
+      ],
+      // transforming the filenames
+      transformPath: function (path) {
+        return path.replace(/\.ts$/, '.js');
+      }
     },
 
 
@@ -50,13 +74,15 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
 
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
